576384893af8d445c9f1412b7ead85f36862a996
Pablo Santana <pablosnta14@hotmail.com>
2024-05-02 11:16:07 +0100

    Recordatorio de medicamento casi listo

--- Code/src/app/app-routing.module.ts
+++ Code/src/app/app-routing.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from "./login/login.component";
-<<<<<<< HEAD
-import { SignUpComponent } from './sign-up/sign-up.component' // Asegúrate de importar el componente SignUpComponent
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from "./home/home.component"; // Se agregó la importación del componente HomeComponent
 import { CreacionRecordatorioComponent } from './creacion-recordatorio/creacion-recordatorio.component';
-=======
-import { SignUpComponent } from './sign-up/sign-up.component'
-import { HomeComponent } from "./home/home.component";
->>>>>>> f1fae6c2982857a578d877d8576b149eed4e2949
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
-  {path: 'creacion-recordatorio', component: CreacionRecordatorioComponent}
+  { path: 'creacion-recordatorio', component: CreacionRecordatorioComponent }
 ];
 
 @NgModule({
--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
 import { CreacionRecordatorioComponent } from './creacion-recordatorio/creacion-recordatorio.component';
-=======
-import { HomeComponent } from './home/home.component';
->>>>>>> f1fae6c2982857a578d877d8576b149eed4e2949
+import { HomeComponent } from './home/home.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-<<<<<<< HEAD
-    CreacionRecordatorioComponent
-=======
+    CreacionRecordatorioComponent,
     HomeComponent
->>>>>>> f1fae6c2982857a578d877d8576b149eed4e2949
   ],
   imports: [
     BrowserModule,

9da65ae9757b8817de4c46063779b5e292607d3b
Marcos Medina <markensi9@outlook.com>
2024-05-09 18:01:30 +0100

    Register elder user

--- Code/src/app/sign-up/sign-up.component.ts
+++ Code/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from "../firebase.service";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+
+  constructor(private firebaseService: FirebaseService) {
+  }
+
+
+  async signupSubmit(event: Event) {
+    event.preventDefault()
+    alert("Se envia")
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const surname = (document.getElementById('surname') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const telephone = (document.getElementById('telephone') as HTMLInputElement).value
+    const password = (document.getElementById('password') as HTMLInputElement).value
+    const language = "ES"
+    const role = "elder"
+
+    this.firebaseService.register(name, surname, email, telephone, password, language, role).subscribe(response => {
+
+    })
+  }
 
 }
